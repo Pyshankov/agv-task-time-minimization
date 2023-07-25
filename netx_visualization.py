057bf51c7a7d4e18f5a0df54a1237fda14ffde23
Pavlo Marchenko <pmarchenko@apple.com>
2023-07-25 15:02:22 +0200

    add names

--- netx_visualization.py
+++ netx_visualization.py
@@ -38,27 +38,39 @@ def draw_graph(graph, agvs):
                 add_edge_to_graph(G, frm, to, 2)
 
         color_map = []
+        positions_labes_names = {}
+        positions_labes = {}
+        aisle_counter = 1
+        operator_counter = 1
         for node in G:
             if graph.cells[node].type in ["OPERATOR_STATION"]:
                     color_map.append('#e0e0eb') 
+                    positions_labes[node] = (positions[node][0], positions[node][1] - 0.2)
+                    positions_labes_names[node] = f'Operator {operator_counter}'
+                    operator_counter = operator_counter + 1
             elif graph.cells[node].type in ["TOTE_PICKUP_STATION"]:
                     color_map.append('#9c9ca4') 
+                    positions_labes[node] = (positions[node][0], positions[node][1] + 0.2)
+                    positions_labes_names[node] =   f'Aisle {aisle_counter}'
+                    aisle_counter = aisle_counter + 1
             else:
+              positions_labes[node] = (positions[node][0], positions[node][1])
+              positions_labes_names[node] = node
               color_map.append('#FBF8F4')
         
         G2 = nx.DiGraph()
     
-        positions2 = {}
+        positions_agv = {}
         edge_agv_route_map = {}
         agv_color_map = []
         strshow = ""
         for agv in agvs:
             strshow = strshow + f'\n AGV {agv.agv_id}:{agv.color} '
             if agv.task is not None:
-                strshow = strshow + f'| TASK: {agv.task.type} {agv.task.status} from:{agv.task.origin} to:{agv.task.destinations1}'
+                strshow = strshow + f'| TASK: {agv.task.type} {agv.task.status} position: {agv.position} route: {agv.ongoing_route}'
 
-            positions2[agv.agv_id] = (agv.x, agv.y)
-            positions2[agv.agv_id + 100] = (agv.x + agv.heading[0], agv.y + agv.heading[1])
+            positions_agv[agv.agv_id] = (agv.x, agv.y)
+            positions_agv[agv.agv_id + 100] = (agv.x + agv.heading[0], agv.y + agv.heading[1])
             G2.add_node(agv.agv_id)
             G2.add_node(agv.agv_id + 100)
             agv_color_map.append(agv.color)
@@ -69,8 +81,8 @@ def draw_graph(graph, agvs):
                  
     plt.clf()
     nx.draw_networkx_nodes(G, pos=positions, node_size=200, node_color=color_map)
-    nx.draw_networkx_labels(G, pos=positions)
-    nx.draw_networkx_nodes(G2, pos=positions2, node_size=[0 if v >= 100 else 400 for v in positions2],node_shape="s", node_color = agv_color_map)
+    nx.draw_networkx_labels(G, pos=positions_labes, labels = positions_labes_names)
+    nx.draw_networkx_nodes(G2, pos=positions_agv, node_size=[0 if v >= 100 else 400 for v in positions_agv],node_shape="s", node_color = agv_color_map)
     # nx.draw_networkx_labels(G2, pos=positions2)
     dashed_edges = graph.edges_adjecent_operator_station_only
     black_edges = [edge for edge in G.edges() if edge not in dashed_edges]
@@ -80,7 +92,7 @@ def draw_graph(graph, agvs):
     nx.draw_networkx_edges(G, pos=positions, edgelist=black_edges, width=list(weights), edge_color='#7D7C7A', arrows=True)
     for color in edge_agv_route_map:
         nx.draw_networkx_edges(G, pos=positions, edgelist=edge_agv_route_map[color], width=2, edge_color=color,style='dashed', arrows=True)
-    nx.draw_networkx_edges(G2, pos=positions2, edgelist=heading_edges, edge_color='#420420', arrows=True)
+    nx.draw_networkx_edges(G2, pos=positions_agv, edgelist=heading_edges, edge_color='#420420', arrows=True)
 
     plt.gca().set_title(strshow, loc='left')
     plt.show()
